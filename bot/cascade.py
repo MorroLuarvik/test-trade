25bfb71bd45208ebfaa5365d80e0b6ad7e302b7e
Mikhail Kozlov <kmo@nkteh.ru>
2019-11-13 17:04:02 +1000

    set default params

--- bot/cascade.py
+++ bot/cascade.py
@@ -488,7 +488,7 @@ class Cascade(AbstractBot):
 	def getParamsTempalte(self):
 		return {
 			"invest": {
-				"default": 0.03983,
+				"default": 17484.0,
 				"mutable": False
 			},
 			"sigmaDays": {
