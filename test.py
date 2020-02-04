2a31c1867f7e849173dac4e18f56fbecc48009f1
Mikhail Kozlov <kmo@nkteh.ru>
2020-02-04 11:13:53 +1000

    remove debug

--- test.py
+++ test.py
@@ -50,7 +50,7 @@ generatons = 32 #last day in evolution 31 + stopTS = 2020.01.16 ?
 startTS = StrToTS("2019.12.12 00:00:00") # startTS = StrToTS("2019.04.08 00:00:00")
 endTS = StrToTS("2020.01.02 00:00:00") # endTS = StrToTS("2019.11.03 00:00:00")
 stopTS = StrToTS("2020.01.03 00:00:00") # stopTS = StrToTS("2019.11.04 00:00:00")
-weightParams = {'profitPercent': {'weight': .45}, 'changeStatusCounter': {'weight': .55, 'maxValue': 15}}
+weightParams = {'profitPercent': {'weight': .45}, 'changeStatusCounter': {'weight': .55, 'maxValue': 10}}
 
 from exchange import Exchange
 from bot import Bot
@@ -168,26 +168,10 @@ for generation in range(generatons):
 	logFile.close()
 
 	# =============== set random params for bots width negative weight =============== #
-	# =============== debug =============== #
-	print('//bot BEFORE remove negative params')
-	for bot in bots:
-		print("//original bot params")
-		print(json.dumps(bot['bot'].getParams()))
-		print("//stored bot params")
-		print(json.dumps(bot['params']))
-		print("\n//bot #{0} start balance: {1}, end balance: {2}, profit: {3}%, weight: {6}, complete date: {4}, change status counts: {5} \r\n".format(bot['bot'].getId(), bot['startBalance'], bot['bot'].getBalance(), bot['profitPercent'], TStoStr(bot['bot'].getChangeStatusTS()), bot['changeStatusCounter'], bot['weight']))
-	# =============== debug =============== #
 	for bot in bots:
 		if bot['weight'] < 0:
 			bot['params'] = mutate.getRandomParams(bot['bot'].getParamsTemplate())
 			bot['weight'] = 0
-	# =============== debug =============== #
-	print('//bot AFTER remove negative params')
-	for bot in bots:
-		print("//randomized bot params")
-		print(json.dumps(bot['params']))
-		print("\n//bot #{0} start balance: {1}, end balance: {2}, profit: {3}%, weight: {6}, complete date: {4}, change status counts: {5} \r\n".format(bot['bot'].getId(), bot['startBalance'], bot['bot'].getBalance(), bot['profitPercent'], TStoStr(bot['bot'].getChangeStatusTS()), bot['changeStatusCounter'], bot['weight']))
-	# =============== debug =============== #
 	# =============== set random params for bots width negative weight =============== #
 
 	# =============== arrange params by profit percent =============== #
