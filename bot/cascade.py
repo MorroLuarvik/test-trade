69d057ffb2faced38797e40d9866d655b487c72c
Morro Luarvik <dr.morro.l@gmail.com>
2020-02-08 13:32:24 +1000

    test yobit  btc rur

--- bot/cascade.py
+++ bot/cascade.py
@@ -488,7 +488,7 @@ class Cascade(AbstractBot):
 	def getParamsTempalte(self):
 		return {
 			"invest": {
-				"default": 24031.,
+				"default": 11747.,
 				"mutable": False
 			},
 			"sigmaDays": {
