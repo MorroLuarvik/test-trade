2c2404c7d3a0971eb36d6c22e74d4e823ae4cce1
speculator <dr.morro.l@gmail.com>
2020-03-23 14:43:09 +1000

    frame for evolution

---
+++ evo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+""" Класс эволюцонного процесса """
+
+class Evo:
+	
+	def init(self, exchangeClass = None, ):
+		pass
+
+	def run(self):
+		pass
+
+	def report(self):
+		pass
