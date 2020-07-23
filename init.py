c88579db042dfb255334bf8dc2392d83586010cf
speculator <dr.morro.l@gmail.com>
2020-07-23 20:13:07 +1000

    check key from another side

--- init.py
+++ init.py
@@ -2,7 +2,7 @@
 #-*-coding:utf-8-*-
 """ инициализация источников данных и подготовка модулей """
 
-from datasource import Datasource, MySQL
+from datasource import Datasource, MySQL #normal
 from configurator.configurator import get_config
 
 Datasource.register_datasource("mysql", MySQL, get_config("mysql"))
